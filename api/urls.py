bb52067aaf01c289895c29e8e57d12dcafebe7b7
daniel <miaramila.malagasy@gmail.com>
2024-07-06 16:58:15 +0300

    First commit

--- api/urls.py
+++ api/urls.py
@@ -11,9 +11,9 @@ router.register(r'technicien', TechnicienViewSet, basename='technicien')
 
 urlpatterns = [
     path('api/get-csrf-token/', get_csrf_token, name='get_csrf_token'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user-info/', UserInfoView.as_view(), name='user_info'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user-info/', UserInfoView.as_view(), name='user_info'),
     path('api/', include(router.urls)),
 ]
