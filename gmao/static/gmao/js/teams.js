211a1be1f691d92816be21b930de07ce3339a6da
daniel <miaramila.malagasy@gmail.com>
2024-07-07 09:15:11 +0300

    First commit

--- gmao/static/gmao/js/teams.js
+++ gmao/static/gmao/js/teams.js
@@ -55,13 +55,14 @@ $(document).ready(function () {
             }
 
             let tableHtml = '<div class="table-responsive">';
-            tableHtml += '<table id="portfolioTable" class="table table-striped">';
+            tableHtml += '<table id="portfolioTable" class="table table-striped" style="width: 100%">';
             tableHtml += '<thead><tr><th>NDIZ</th><th>Station</th><th>Élément</th><th>Panne</th><th>Statut</th><th>Actions</th></tr></thead><tbody>';
 
             const hasOngoingIntervention = data.some(doleance => doleance.statut === 'ATT' || doleance.statut === 'INT');
 
             data.forEach(function (doleance) {
                 tableHtml += `<tr>
+                <td></td>
                 <td>${doleance.ndi}</td>
                 <td>${doleance.station}</td>
                 <td>${doleance.element}</td>
@@ -74,7 +75,7 @@ $(document).ready(function () {
             tableHtml += '</tbody></table></div>';
             $('#portfolioContainer').html(tableHtml);
 
-            $('#portfolioTable').DataTable({
+            /*$('#portfolioTable').DataTable({
                 responsive: true,
                 autoWidth: false,
                 language: {
@@ -91,6 +92,35 @@ $(document).ready(function () {
                 buttons: [
                     'copy', 'excel', 'pdf'
                 ]
+            });*/
+            $('#portfolioTable').DataTable({
+                responsive: {
+                    details: {
+                        type: 'column',
+                        target: 'tr'
+                    }
+                },
+                autoWidth: false,
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.10.24/i18n/French.json'
+                },
+                columnDefs: [
+                    {
+                        className: 'control',
+                        orderable: false,
+                        targets: 0
+                    },
+                    {responsivePriority: 1, targets: 1}, // NDIZ
+                    {responsivePriority: 2, targets: -1}, // Actions
+                    {responsivePriority: 3, targets: 5}, // Statut
+                    {width: "25%", targets: 4, className: 'panne-cell'}, // Panne
+                    {width: "15%", targets: -1, className: 'action-cell'} // Actions
+                ],
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'excel', 'pdf'
+                ],
+                order: [[1, 'asc']]
             });
         }
     }
