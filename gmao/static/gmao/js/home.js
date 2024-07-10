48aa75b1d6a3c42d73d6cb3b2c34da46f3d00007
daniel <miaramila.malagasy@gmail.com>
2024-07-10 21:24:13 +0300

    Version Alpha

--- gmao/static/gmao/js/home.js
+++ gmao/static/gmao/js/home.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
         offsetAnchor();
     }, 1);
 
-    function initDoleanceTable() {
+    /*function initDoleanceTable() {
         if ($('#demandeencours').length && !$.fn.DataTable.isDataTable('#demandeencours')) {
             doleanceTable = $('#demandeencours').DataTable({
                 ajax: {
@@ -95,6 +95,68 @@ $(document).ready(function () {
                     {responsivePriority: 10000, targets: [2, 4, 5, 7]}
                 ]
             });
+        }
+    }*/
+    function initPortfolioTable(data, interventionEnCoursGlobal) {
+        console.log("Initialisation de la table avec les données:", data);
+        if ($('#portfolioTable').length) {
+            if ($.fn.DataTable.isDataTable('#portfolioTable')) {
+                $('#portfolioTable').DataTable().destroy();
+            }
+
+            try {
+                $('#portfolioTable').DataTable({
+                    data: data,
+                    columns: [
+                        // ... vos colonnes existantes ...
+                    ],
+                    responsive: {
+                        details: {
+                            type: 'column',
+                            target: 'tr',
+                            renderer: function (api, rowIdx, columns) {
+                                var data = $.map(columns, function (col, i) {
+                                    return col.hidden ?
+                                        '<tr data-dt-row="' + col.rowIndex + '" data-dt-column="' + col.columnIndex + '">' +
+                                        '<td>' + col.title + ':' + '</td> ' +
+                                        '<td>' + col.data + '</td>' +
+                                        '</tr>' :
+                                        '';
+                                }).join('');
+                                return data ?
+                                    $('<table/>').append(data) :
+                                    false;
+                            }
+                        }
+                    },
+                    autoWidth: false,
+                    ordering: false,
+                    language: {
+                        url: '//cdn.datatables.net/plug-ins/1.10.24/i18n/French.json'
+                    },
+                    pageLength: -1,  // Affiche toutes les entrées
+                    lengthChange: false,  // Supprime le sélecteur de nombre d'entrées
+                    drawCallback: function (settings) {
+                        console.log("DrawCallback - Intervention en cours (global):", interventionEnCoursGlobal);
+                        $('.prendre-en-charge').prop('disabled', interventionEnCoursGlobal);
+                    },
+                    columnDefs: [
+                        {
+                            targets: 0,
+                            className: 'control',
+                            orderable: false,
+                            render: function () {
+                                return '<i class="fa fa-plus-square" aria-hidden="true"></i>';
+                            }
+                        },
+                        // ... vos autres columnDefs ...
+                    ]
+                });
+            } catch (error) {
+                console.error("Erreur lors de l'initialisation de la table:", error);
+            }
+        } else {
+            console.error("L'élément #portfolioTable n'existe pas dans le DOM");
         }
     }
 
