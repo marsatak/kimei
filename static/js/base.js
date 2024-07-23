27e4e652248cd3f3be800dcd60bd0a224d589af6
daniel <miaramila.malagasy@gmail.com>
2024-07-23 21:02:28 +0300

    Version Alpha Navbar avec équipements

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var equipmentDropdown = document.getElementById('navbarDropdownEquipements');
-    if (equipmentDropdown) {
-        console.log("Dropdown trouvé, initialisation...");
-        new bootstrap.Dropdown(equipmentDropdown);
-    } else {
-        console.log("Dropdown non trouvé");
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var equipmentsDropdown = document.getElementById('equipmentsDropdown');
+    var navbarCollapse = document.getElementById('navbarNav');
+
+    function toggleEquipmentsDropdown() {
+        if (window.innerWidth <= 991) { // Si en mode mobile
+            equipmentsDropdown.classList.add('show');
+        } else {
+            equipmentsDropdown.classList.remove('show');
+        }
     }
+
+    // Développer le dropdown "Équipements" par défaut en mode mobile
+    toggleEquipmentsDropdown();
+
+    // Ajuster lorsque la fenêtre est redimensionnée
+    window.addEventListener('resize', toggleEquipmentsDropdown);
+
+    // Assurer la fermeture des autres dropdowns lorsqu'on clique en dehors
+    document.addEventListener('click', function (event) {
+        var isClickInside = navbarCollapse.contains(event.target);
+        if (!isClickInside) {
+            document.querySelectorAll('.dropdown-menu.show').forEach(function (openDropdown) {
+                openDropdown.classList.remove('show');
+            });
+        }
+    });
+
+    // Assurer la fermeture des autres dropdowns lorsqu'on utilise le bouton hamburger
+    navbarToggler.addEventListener('click', function () {
+        setTimeout(function () {
+            document.querySelectorAll('.dropdown-menu.show').forEach(function (openDropdown) {
+                openDropdown.classList.remove('show');
+            });
+            toggleEquipmentsDropdown(); // Re-développer le dropdown "Équipements" si en mode mobile
+        }, 300);
+    });
 });
