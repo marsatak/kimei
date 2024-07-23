3a7fbf56dcca5d111c7ae32e2d6338e404473cb6
daniel <miaramila.malagasy@gmail.com>
2024-07-23 20:05:27 +0300

    Version Alpha Navbar avec équipements

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-    var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-        return new bootstrap.Dropdown(dropdownToggleEl)
-    })
+    var equipmentDropdown = document.getElementById('navbarDropdownEquipements');
+    if (equipmentDropdown) {
+        console.log("Dropdown trouvé, initialisation...");
+        new bootstrap.Dropdown(equipmentDropdown);
+    } else {
+        console.log("Dropdown non trouvé");
+    }
 });
